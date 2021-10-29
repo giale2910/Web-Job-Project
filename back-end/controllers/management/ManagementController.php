5aeaf56acb284817e9afc1102e05ac87e25cca8c
tmsang2000 <sang.trieu@netpower.no>
2021-10-29 14:40:09 +0700

    Update separate css component

--- back-end/controllers/management/ManagementController.php
+++ back-end/controllers/management/ManagementController.php
@@ -10,20 +10,20 @@ class ManagementController extends BaseController
     {
         $data["title"] = "ManagementPostJob";
         $data["cssFiles"] = [
-            "css/customer/imported/bootstrap.min.css",
-            "css/customer/imported/animate.min.css",
-            "css/customer/imported/bootstrap-submenu.css",
-            "css/customer/imported/bootstrap-select.min.css",
-            "css/customer/imported/magnific-popup.css",
-            "css/customer/imported/daterangepicker.css",
-            "css/customer/imported/leaflet.css",
-            "css/customer/imported/map.css",
-            "css/customer/imported/jquery.mCustomScrollbar.css",
-            "css/customer/imported/dropzone.css",
-            "css/customer/imported/slick.css",
-            "css/customer/imported/style.css",
-            "css/customer/imported/skins/midnight-blue.css",
-            "css/customer/imported/ie10-viewport-bug-workaround.css",
+            // "css/customer/imported/bootstrap.min.css",
+            // "css/customer/imported/animate.min.css",
+            // "css/customer/imported/bootstrap-submenu.css",
+            // "css/customer/imported/bootstrap-select.min.css",
+            // "css/customer/imported/magnific-popup.css",
+            // "css/customer/imported/daterangepicker.css",
+            // "css/customer/imported/leaflet.css",
+            // "css/customer/imported/map.css",
+            // "css/customer/imported/jquery.mCustomScrollbar.css",
+            // "css/customer/imported/dropzone.css",
+            // "css/customer/imported/slick.css",
+            // "css/customer/imported/style.css",
+            // "css/customer/imported/skins/midnight-blue.css",
+            // "css/customer/imported/ie10-viewport-bug-workaround.css",
         ];
         $data["jsFiles"] = [
             
