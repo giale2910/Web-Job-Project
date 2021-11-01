b648e4e3380576d8a5e2ee89338c11f79aca18ca
KietB <kiet.buibui@hcmut.edu.vn>
2021-11-01 17:02:00 +0700

    Initialize DB config

--- back-end/configs/database.php
+++ back-end/configs/database.php
@@ -1,6 +1,37 @@
 <?php
 $database["host"] = "localhost";
 $database["port"] = 3306;
-$database["databasename"] = "food_shop";
+$database["databasename"] = "job_finding";
 $database["username"] = "root";
-$database["password"] = "Abc.123456";
+$database["password"] = "root";
+$database["version"] = "1.2.3";
+$database["admins"] = array(
+    array(
+        "email" => "admin@mail.com",
+        "password" => "admin",
+        "first_name" => "Andy",
+        "last_name" => "Min"
+    )
+);
+$database["managers"] = array(
+    array(
+        "email" => "manager1@yahoo.com",
+        "password" => "123456",
+        "first_name" => "Mike",
+        "last_name" => "Hunt"
+    ),
+    array(
+        "email" => "manager2@gmail.com",
+        "password" => "monkey",
+        "first_name" => "Brad-Jonas",
+        "last_name" => "Hardick"
+    )
+);
+$database["customers"] = array(
+    array(
+        "email" => "user1@hcmut.edu.vn",
+        "password" => "1011101",
+        "first_name" => "A",
+        "last_name" => "B"
+    )
+);
